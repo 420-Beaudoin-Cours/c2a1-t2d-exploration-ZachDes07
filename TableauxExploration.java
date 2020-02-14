f13609b12e18bf035fd5a696612c688bf8b1653f
ZachDes07 <zacou07@hotmail.com>
2020-02-14 11:05:51 -0500

    First

---
+++ TableauxExploration.java
@@ -0,0 +1,27 @@
+public class TableauxExploration {
+
+    public static void initTen(int[][] tabResult) {
+
+    }
+
+    public static void initDiagonaleTen(int[][] tabResult) {
+
+    }
+
+    public static void initPosition(String[][] tabResult) {
+
+    }
+
+    public static Object isTabCarre(int[][] tab) {
+    }
+
+
+    public static Object isTabDimEgal(int[][] tab1, int[][] tab2) {
+
+    }
+
+    public static int[][] getSommeMatrices(int[][] matrice1, int[][] matrice2) {
+        
+    }
+}
+
